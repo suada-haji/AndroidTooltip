8edb2311277fd47a9bef280f297a2b03ed66567a
Mokanarangan <Mokanarangan.Thayaparan@tryacke.com>
2017-05-22 22:30:27 +0530

    added

--- app/src/main/java/com/example/suadahaji/androidtooltip/MainActivity.java
+++ app/src/main/java/com/example/suadahaji/androidtooltip/MainActivity.java
@@ -1,27 +1,58 @@
 package com.example.suadahaji.androidtooltip;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.ActionViewTarget;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Toolbar toolbar;
+    private MenuItem mQueueMenuItem;
+    ShowcaseView sv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewTarget target = new ViewTarget(R.id.anchor_view, this);
+        RelativeLayout.LayoutParams lps = new RelativeLayout
+                .LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        sv = new ShowcaseView.Builder(this)
+                .withMaterialShowcase()
+                .setTarget(target)
+                .setContentTitle("Content")
+                .build();
+        sv.setButtonPosition(lps);
+        sv.show();
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        mQueueMenuItem = menu.findItem(R.id.filter);
         return true;
+    }
+
+    private void showIntroductoryOverlay() {
+        new ShowcaseView.Builder(this)
+                .setTarget(new ActionViewTarget(this, ActionViewTarget.Type.HOME))
+                .setContentTitle("ShowcaseView")
+                .setContentText("This is highlighting the Home button")
+                .hideOnTouchOutside()
+                .build().show();
     }
 
     @Override
